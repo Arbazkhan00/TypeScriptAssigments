296a474c7c2462e1df866a972552f81c976a3736
Arbaz khan <arbazjani8@gmail.com>
2023-02-19 18:23:22 +0500

    Q No 5

---
+++ Q No 5/index.js
@@ -0,0 +1,3 @@
+var famous_person = "Thomas A. Edison";
+var massage = "The Famous Personf Says ".concat(famous_person, " \"Many of life's failures are people who did not realize how close they were to success when they gave up\"");
+console.log(massage);
---
+++ Q No 5/index.ts
@@ -0,0 +1,3 @@
+let famous_person="Thomas A. Edison";
+let massage=`The Famous Personf Says ${famous_person} "Many of life's failures are people who did not realize how close they were to success when they gave up"`;
+console.log(massage);
