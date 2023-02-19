db7160762e675946dbf4a5fe06b19c96f20f82f9
Arbaz khan <arbazjani8@gmail.com>
2023-02-20 00:06:56 +0500

    Q No 6

---
+++ Q No 6/index.js
@@ -0,0 +1,3 @@
+var nameg = "\t\n  John Doe  \n\t";
+console.log("Name with whitespace: \"".concat(nameg, "\""));
+console.log("Stripped name: \"".concat(nameg.trim(), "\""));
---
+++ Q No 6/index.ts
@@ -0,0 +1,3 @@
+const nameg= "\t\n  John Doe  \n\t";
+console.log(`Name with whitespace: "${nameg}"`);
+console.log(`Stripped name: "${nameg.trim()}"`);
