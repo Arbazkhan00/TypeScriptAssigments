5a5e4d17ad69d5b815f5f641cde6fdbaccd1811c
Arbaz khan <arbazjani8@gmail.com>
2023-02-20 17:21:58 +0500

    Q No 12/

---
+++ Q No 12/index.js
@@ -0,0 +1,5 @@
+var hello = ['Adil', 'Haris', 'Usama', 'Moshin'];
+console.log("Hi ".concat(hello[0], ",What are you doing there"));
+console.log("Hi ".concat(hello[1], ",What are you doing there"));
+console.log("Hi ".concat(hello[2], ",What are you doing there"));
+console.log("Hi ".concat(hello[3], ",What are you doing there"));
---
+++ Q No 12/index.ts
@@ -0,0 +1,5 @@
+let hello=['Adil','Haris','Usama','Moshin'];
+console.log(`Hi ${hello[0]},What are you doing there`);
+console.log(`Hi ${hello[1]},What are you doing there`);
+console.log(`Hi ${hello[2]},What are you doing there`);
+console.log(`Hi ${hello[3]},What are you doing there`);
