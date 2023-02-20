34c807da8a413b58638629cce0500b9e860fc183
Arbaz khan <arbazjani8@gmail.com>
2023-02-20 17:14:38 +0500

    Q No 11/

---
+++ Q No 11/index.js
@@ -0,0 +1,5 @@
+var arry = ['Haris', 'adil', 'sardari', 'usama'];
+console.log(arry[0]);
+console.log(arry[1]);
+console.log(arry[2]);
+console.log(arry[3]);
---
+++ Q No 11/index.ts
@@ -0,0 +1,5 @@
+let arry=['Haris','adil','sardari','usama'];
+console.log(arry[0]);
+console.log(arry[1]);
+console.log(arry[2]);
+console.log(arry[3]);
