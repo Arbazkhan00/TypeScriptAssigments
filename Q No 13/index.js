f48383789fe697362ffc1e9a7e0c4457e524b038
Arbaz khan <arbazjani8@gmail.com>
2023-02-20 18:00:41 +0500

    Q No 13/

---
+++ Q No 13/index.js
@@ -0,0 +1,6 @@
+var vehicles = ["car", "motorcycle", "bicycle", "scooter", "bus"];
+console.log("I would like to own a ".concat(vehicles[0], "."));
+console.log("I would like to own a ".concat(vehicles[1], "."));
+console.log("I would like to own a ".concat(vehicles[2], "."));
+console.log("I would like to own a ".concat(vehicles[3], "."));
+console.log("I would like to own a ".concat(vehicles[4], "."));
---
+++ Q No 13/index.ts
@@ -0,0 +1,7 @@
+const vehicles = ["car", "motorcycle", "bicycle", "scooter", "bus"]
+  console.log(`I would like to own a ${vehicles[0]}.`);
+  console.log(`I would like to own a ${vehicles[1]}.`);
+  console.log(`I would like to own a ${vehicles[2]}.`);
+  console.log(`I would like to own a ${vehicles[3]}.`);
+  console.log(`I would like to own a ${vehicles[4]}.`);
+
