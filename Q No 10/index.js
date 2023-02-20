627e0a375bf3ebc986bc8b4cba235020274fbc41
Arbaz khan <arbazjani8@gmail.com>
2023-02-20 16:41:58 +0500

    Q No 10/

---
+++ Q No 10/index.js
@@ -0,0 +1,18 @@
+/*There is a program for addition,substraction,muktiplication,division
+Name Arbaz khan
+Date 20.2.2023
+time 4:36 pm
+*/
+console.log("Addition : ", 4 + 4);
+console.log("Multiplication: ", 2 * 4);
+console.log("Substraction: ", 10 - 2);
+console.log("Division: ", 16 / 2);
+/*There is a program for Name writtin in uper,lower and title case
+Name Arbaz khan
+Date 20.2.2023
+time 4:36 pm
+*/
+var name22 = "Arbaz khan";
+console.log("Upper Case: " + name22.toUpperCase());
+console.log("Lower Case: " + name22.toLowerCase());
+console.log("Title Case: " + name22.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); }));
