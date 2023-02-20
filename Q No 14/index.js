f121294757579224d8477b98152233aeec41dbec
Arbaz khan <arbazjani8@gmail.com>
2023-02-20 23:33:35 +0500

    Q No 14/

---
+++ Q No 14/index.js
@@ -0,0 +1,4 @@
+var guestList = ['Adil', 'haris', 'Usama'];
+console.log("Dear ".concat(guestList[0], ", you are cordially invited to a dinner at my place. Please let me know if you can attend."));
+console.log("Dear ".concat(guestList[1], ", you are cordially invited to a dinner at my place. Please let me know if you can attend."));
+console.log("Dear ".concat(guestList[2], ", you are cordially invited to a dinner at my place. Please let me know if you can attend."));
