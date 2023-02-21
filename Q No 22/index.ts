3a18ff5c778be5d441d3b47a5aec5298aa295cb6
Arbaz khan <arbazjani8@gmail.com>
2023-02-21 23:03:57 +0500

    Q No 22/

---
+++ Q No 22/index.ts
@@ -0,0 +1,7 @@
+ // const fruits = ['apple', 'banana', 'orange'];
+ // console.log(fruits[3]);  // trying to access an element at index 3 which does not exist
+// undefined
+// RangeError: Invalid array length
+//     at file:///home/user/code/index.ts:2:13
+const fruits = ['apple', 'banana', 'orange'];
+console.log(fruits[2]);  // accessing the third element at index 2
