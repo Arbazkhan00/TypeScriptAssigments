139990d3ca1d59bfe7dd34895291b216ff1c8dbd
Arbaz khan <arbazjani8@gmail.com>
2023-02-21 22:59:32 +0500

    Q No 21/

---
+++ Q No 21/index.js
@@ -0,0 +1,18 @@
+var person1 = {
+    name: "Ali",
+    age: 25,
+    city: "Afghanistan"
+};
+var person2 = {
+    name: "Haris",
+    age: 30,
+    city: "Israil"
+};
+var person3 = {
+    name: "Arbaz khan",
+    age: 35,
+    city: "Pakistan"
+};
+console.log(person1);
+console.log(person2);
+console.log(person3);
---
+++ Q No 21/index.ts
@@ -0,0 +1,28 @@
+interface Person {
+    name: string;
+    age: number;
+    city: string;
+  }
+  
+  const person1: Person = {
+    name: "Ali",
+    age: 25,
+    city: "Afghanistan"
+  };
+  
+  const person2: Person = {
+    name: "Haris",
+    age: 30,
+    city: "Israil"
+  };
+  
+  const person3: Person = {
+    name: "Arbaz khan",
+    age: 35,
+    city: "Pakistan"
+  };
+  
+  console.log(person1);
+  console.log(person2);
+  console.log(person3);
+  
