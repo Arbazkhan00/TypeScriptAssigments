da0e9d4d39165d5642f324bfb23abf77702cab31
Arbaz khan <arbazjani8@gmail.com>
2023-02-21 23:37:24 +0500

    Q No 25/

---
+++ Q No 25/index.ts
@@ -0,0 +1,11 @@
+let alien_color: string = 'green';
+
+if (alien_color === 'green') {
+    console.log('Congratulations! You just earned 5 points.');
+    let alien_color2: string = 'red';
+
+if (alien_color2 === 'green') {
+    console.log('Congratulations! You just earned 5 points.');
+}
+}
+
