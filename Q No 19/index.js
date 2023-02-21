200f86b1d1f6ee237c1be6c629ea48d5c6b9e602
Arbaz khan <arbazjani8@gmail.com>
2023-02-21 22:49:36 +0500

    Q No 19/

---
+++ Q No 19/index.js
@@ -0,0 +1,2 @@
+var guests = ["Adil", "Usama", "Haris"];
+console.log("I am inviting ".concat(guests.length, " guests to dinner."));
---
+++ Q No 19/index.ts
@@ -0,0 +1,2 @@
+let guests: string[] = ["Adil", "Usama", "Haris"];
+console.log(`I am inviting ${guests.length} guests to dinner.`);
