1280414e2f71fe9ee4cf5c206e7d276eb1a8aafa
Arbaz khan <arbazjani8@gmail.com>
2023-02-25 11:47:42 +0500

    Q No 44

---
+++ Q No 44/index.ts
@@ -0,0 +1,8 @@
+function makeSandwich(...items: string[]): void {
+    console.log(`Making a sandwich with: ${items.join(', ')}`);
+  }
+  
+  makeSandwich('turkey', 'lettuce', 'tomato', 'mayo');
+  makeSandwich('ham', 'cheese', 'mustard');
+  makeSandwich('peanut butter', 'jelly');
+  
