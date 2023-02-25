aadfc7cb53f4dbcdceb7fb65627d3531a20e4406
Arbaz khan <arbazjani8@gmail.com>
2023-02-25 10:42:53 +0500

    Q No 26/

---
+++ Q No 26/index.ts
@@ -0,0 +1,8 @@
+
+const alienColor: string = "green";
+if (alienColor === "green") {
+  console.log("Congratulations! You just earned 5 points for shooting the alien.");
+}
+if (alienColor !== "green") {
+  console.log("Congratulations! You just earned 10 points.");
+}
