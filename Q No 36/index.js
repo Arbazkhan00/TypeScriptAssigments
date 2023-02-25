0fbbf7ba3fca475f9fa98067b4e4fbd8473998e4
Arbaz khan <arbazjani8@gmail.com>
2023-02-25 11:46:18 +0500

    Q No 36/

---
+++ Q No 36/index.js
@@ -0,0 +1,4 @@
+function make_shirt13(size, message) {
+    console.log("You have ordered a ".concat(size, " t-shirt with the message: \"").concat(message, "\"."));
+}
+make_shirt13("M", "I love TypeScript!");
---
+++ Q No 36/index.ts
@@ -0,0 +1,5 @@
+function make_shirt13(size: string, message: string): void {
+    console.log(`You have ordered a ${size} t-shirt with the message: "${message}".`);
+  }
+  
+  make_shirt13("M", "I love TypeScript!"); 
