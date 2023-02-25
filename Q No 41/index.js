a15a6ac508e75bfdbbc796be4630f5f77cf189eb
Arbaz khan <arbazjani8@gmail.com>
2023-02-25 11:47:13 +0500

    Q No 41

---
+++ Q No 41/index.js
@@ -0,0 +1,8 @@
+function show_magicians13(magicians13) {
+    for (var _i = 0, magicians13_1 = magicians13; _i < magicians13_1.length; _i++) {
+        var magician = magicians13_1[_i];
+        console.log(magician);
+    }
+}
+var magicians13 = ["Harry Houdini", "David Blaine", "Penn Jillette", "Teller"];
+show_magicians13(magicians13);
---
+++ Q No 41/index.ts
@@ -0,0 +1,9 @@
+function show_magicians13(magicians13: string[]): void {
+    for (let magician of magicians13) {
+      console.log(magician);
+    }
+  }
+  
+  let magicians13: string[] = ["Harry Houdini", "David Blaine", "Penn Jillette", "Teller"];
+  show_magicians13(magicians13);
+  
