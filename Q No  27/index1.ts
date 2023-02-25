9eb6759f82020ca068510d6bfaa1b0654df18c05
Arbaz khan <arbazjani8@gmail.com>
2023-02-25 10:51:27 +0500

    Q No 27/

---
+++ Q No  27/index1.ts
@@ -0,0 +1,12 @@
+
+const alienColor2: string = "green";
+
+if (alienColor2 === "green") {
+  console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+} else if (alienColor2 === "yellow") {
+  console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+} else if (alienColor2 === "red") {
+  console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+} else {
+  console.log("Unknown alien color. No points earned.");
+}
---
+++ Q No  27/index3.ts
@@ -0,0 +1,12 @@
+
+const alienColor4: string = "red";
+
+if (alienColor4 === "green") {
+  console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+} else if (alienColor4 === "yellow") {
+  console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+} else if (alienColor4 === "red") {
+  console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+} else {
+  console.log("Unknown alien color. No points earned.");
+}
